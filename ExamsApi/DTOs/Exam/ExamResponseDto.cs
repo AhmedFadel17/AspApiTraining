f8429fe0ed2c3aeab92ed87a9226c60809b8dc77
AhmedFadel17 <ahmedfadel1717@gmail.com>
2025-01-11 21:48:53 +0200

    add response DTOs

---
+++ ExamsApi/DTOs/Exam/ExamResponseDto.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json.Serialization;
+using ExamsApi.Models;
+namespace ExamsApi.DTOs.Exam
+{
+    public class ExamResponseDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Grade { get; set; }
+        public string Subject { get; set; }
+        public int Time { get; set; }
+        public double TotalMarks { get; set; }
+    }
+}
---
+++ ExamsApi/DTOs/ExamModel/ExamModelResponseDto.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ExamsApi.DTOs.ExamModel
+{
+    public class ExamModelResponseDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int ExamId { get; set; }
+    }
+}
