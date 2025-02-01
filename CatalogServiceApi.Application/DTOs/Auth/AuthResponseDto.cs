32a9eff7fe1ba568cf40a1698056c070be4c1395
AhmedFadel17 <ahmedfadel1717@gmail.com>
2025-02-02 01:06:26 +0200

    remove auth dtos

--- CatalogServiceApi.Application/DTOs/Auth/AuthResponseDto.cs
+++
@@ -1,9 +0,0 @@
-﻿
-namespace CatalogServiceApi.Application.DTOs.Auth
-{
-    public record AuthResponseDto
-    {
-        public string Token { get; set; } = string.Empty;
-        public UserDto User { get; set; } = new UserDto();
-    }
-}
--- CatalogServiceApi.DataAccess/Data/ApplicationDbContext.cs
+++ CatalogServiceApi.DataAccess/Data/ApplicationDbContext.cs
@@ -6,7 +6,6 @@ namespace CatalogServiceApi.DataAccess.Data
     public class ApplicationDbContext : DbContext
     {
         private readonly ApplicationDbContext _context;
-        public DbSet<User> Users { get; set; }
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
