f76282cde446194753d5bbe6322f92764e25c55d
Mahmoud Fadel <Mahmoud_Fadel@epam.com>
2025-02-05 16:51:32 +0100

    fix: Add ExcludeFromCodeCoverage attr

--- CatalogServiceApi.Domain/Models/Category.cs
+++ CatalogServiceApi.Domain/Models/Category.cs
@@ -1,7 +1,9 @@
-﻿using System.Text.Json.Serialization;
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Text.Json.Serialization;
 
 namespace CatalogServiceApi.Domain.Models
 {
+    [ExcludeFromCodeCoverage]
     public class Category
     {
         public int Id { get; set; }
